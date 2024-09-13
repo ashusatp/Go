64f2de0c4ea36bab8c3f3e3a2f303479657ef8ef
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-13 21:30:46 +0530

    simple values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//int
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("hello world")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// floats
+	fmt.Println(10.5)
+
+	fmt.Println(7.0 / 3.0)
+
+}
