77a8260e20c6d3b788a2133342d1f88428f4b3e0
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-13 23:16:14 +0530

    switch update

--- 7_switch/switch.go
+++ 7_switch/switch.go
@@ -22,12 +22,27 @@ func main() {
 	}
 
 	// multiple condition switch
-
 	switch time.Now().Weekday() {
 	case time.Saturday, time.Sunday:
 		fmt.Println("it's weekend")
 	default:
-
+		fmt.Println("it's workday")
 	}
+
+	// type switch
+	whoAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case int:
+			fmt.Println("an Integer")
+		case string:
+			fmt.Println("a string")
+		case bool:
+			fmt.Println("a boolean")
+		default:
+			fmt.Println("other", t)
+		}
+	}
+
+	whoAmI("golang")
 
 }
