cac7e6b87a910dfef3ac67344f566ce9f5243d3f
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-13 21:37:25 +0530

    variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// var name string = "golang"
+	// infer
+	// var name = "golang"
+	// sorthand syntax
+	name := "golang"
+
+	var newName string
+	newName = "Ashutosh"
+
+	var isExist bool = false
+
+	var intPrice int = 50
+	var price float32 = 50.5
+
+	fmt.Println(name)
+	fmt.Println(isExist)
+	fmt.Println(price)
+	fmt.Println(intPrice)
+	fmt.Println(newName)
+
+}
