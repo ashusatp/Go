bd84846fa09526949f9d2a1cd52dd6311a197e9f
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-14 12:20:18 +0530

    pointers update

--- 14_pointers/pointers.go
+++ 14_pointers/pointers.go
@@ -13,15 +13,23 @@ func refChange(num *int) {
 	*num = 5
 }
 
+func changeSlices(nums *[]int) {
+	*nums = append(*nums, 5)
+}
+
 func main() {
 
 	num := 1
 	changenum(num)
 
-	fmt.Println("After change number in main function", num)
+	fmt.Println("number in main function", num)
 
 	// pointers(pass by refrence)
 	refChange(&num)
 	fmt.Println("number address", &num)
 
+	// -> refrence in for slices
+	nums := []int{1, 2, 3}
+	changeSlices(&nums)
+	fmt.Println(nums)
 }
