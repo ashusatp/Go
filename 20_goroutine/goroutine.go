6dae25b073739aa05436b664d2ca8911116fc6e5
ashusatp <92241390+ashusatp@users.noreply.github.com>
2024-09-17 02:03:35 +0530

    goroutine

---
+++ 20_goroutine/goroutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task(id int) {
+	fmt.Println("doing task", id)
+}
+
+func main() {
+
+	for i := 0; i < 100; i++ {
+
+		// go task(i)
+
+		go func(i int) {
+			fmt.Println("doing task", i)
+		}(i)
+
+	}
+
+	time.Sleep(time.Second * 2)
+}
